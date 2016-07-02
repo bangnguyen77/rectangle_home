75954320c9cf770ba5b0e25c3c5a2baaa94aa931
Bang Nguyen <bangnv77@gmail.com>
2016-07-02 13:17:39 -0700

    add lib and spec folders and files

---
+++ lib/rectangle.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('rectangle')
+
+class Rectangle
+  define_method(:initialize) do |length, width|
+    @length = length
+    @width = width
+  end
+
+  define_method(:square?) do
+    @length.eql?(@width)
+  end
+end
